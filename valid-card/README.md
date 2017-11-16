387508b8b45d99931c97a0dce932f36010ecdcf0
Eri <erika@MacBook-Pro-de-Erika.local>
2017-11-16 10:50:45 -0600

    para revisión del Cipher

--- valid-card/README.md
+++ valid-card/README.md
@@ -1,1 +1,13 @@
+## Tarjeta valida
 
+1. pedir un número de tarjeta de 16 digitos.
+2.
+
+
+
+
+a los índices impares los multiplicamos por 2
+si el resultado el mayor a 10 hay que sumar sus digitos
+separar los digitos
+umar sus digitosguardar el resultado en la posición numero original
+si no es mayor a 10 sumarlo al resutado final
--- valid-card/js/app.js
+++ valid-card/js/app.js
@@ -1,1 +1,13 @@
+do {
+  var tarjeta = prompt("Ingrese su número de tarjeta de 16 digitos");
+  if (tarjeta === '' || parseInt(tarjeta)==0 NaN || tarjeta.length !==16) {
+    alert ('Tarjeta no valida');
+  }
+  while (parseInt (tarjeta) === NaN || tarjeta.length !== 10)
+  console.log(tarjeta);
 
+  function luhn (numeroDeTarjeta) {
+    var arregloNumero
+  }
+  }
+}
